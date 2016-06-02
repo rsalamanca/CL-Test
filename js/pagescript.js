8416a8faca9ca62f5a30aea21f2fd65fbeedecdb
Ronito <ronito.j@hotmail.com>
2016-06-02 15:09:58 -0400

    Working on Cleaning up JavaScript - Organizing Chart
    Reworked on some code to remove a function using global variables. I've
    created a temporary storage variable that will only call on the function
    once instead of 3 times.

--- js/pagescript.js
+++ js/pagescript.js
@@ -1,3 +1,13 @@
+function maxNumber(arrayNumbers) {
+   var highest = -1;
+      for (var i = 0; i < arrayNumbers.length; i++) {
+         if (arrayNumbers[i] > highest) {
+            highest = arrayNumbers[i];
+         }
+      };
+      return highest;
+   }
+ 
 function maxNumberElement(className) {
    var highest = -1;
    var i = 0;
@@ -18,16 +28,237 @@ function sameHeight(block1, block2) {
    }
 }
 
+function jsonToDataPoints(axisX, axisY, labelFormat) {
+   var maxY = maxNumber(axisY) + 200;
+   var dataPointsBackground = []; 
+   var dataPointsMain = [];
+   
+   for (var i = 0; i < axisX.length; i++) {
+      //DataPoints for color change of entire background
+      dataPointsBackground.push({
+         y: maxY,
+         x: axisX[i]
+      });
+
+      //DataPoints for main section
+      dataPointsMain.push({
+         y: axisY[i],
+         x: axisX[i],
+         label: labelFormat[i]
+      });
+   }
+   
+   return [dataPointsBackground, dataPointsMain, maxY];
+}
+
+function onMouseover(e) {
+   e.dataPoint.markerColor = "#53BBB3";
+   e.dataPoint.markerBorderThickness = 0;
+}
+
+//Vertical Lines and fill
+function verticalLineChart(arrayDataPoints, canvasChart) {
+   var axisY = [];
+   var axisX = [];
+   var dataInsert = [];
+
+   for (var i = 0; i < arrayDataPoints.length; i++) {
+      axisX[i] = arrayDataPoints[i].x;
+      dataInsert[i] = {
+         type: "line",
+         lineColor: "#e8e9e9",
+         lineThickness: 1,
+         markerBorderThickness: false,
+         markerSize: 0,
+         tooltipContent: null,
+
+         dataPoints: [
+
+            {
+               y: arrayDataPoints[i].y,
+               x: arrayDataPoints[i].x,
+               highlightEnabled: false,
+               toolTipContent: null,
+               lineDashType: "longDash"
+            }, {
+               y: 0,
+               x: arrayDataPoints[i].x,
+               highlightEnabled: false,
+               toolTipContent: null
+            },
+         ]
+      };
+      canvasChart.options.data.push(dataInsert[i]);
+   }
+};
+
 function verticalAlignThis(alignThis) {
    alignThis.css({
-      marginTop: (alignThis.parent().height() - alignThis.height()) / 2,
+      marginTop: (alignThis.parent().height() - alignThis.height()) / 2
    });
 }
 
 $(document).ready(function() {
    var windowWidth = window.innerWidth;
+   //Slider Variables
    var maxHeightSlides = 0;
-      
+
+   //Chart Variables
+   var axisYValue = [];
+   var axisXValue = [];
+   var labelValue = [];
+   var dataPointsCover = [];
+   var dataPointsValue = [];
+   var maxYAxis = 0;
+   var viewportMin = 0;
+   var viewportMax = 11;
+   var currentPos = 0;
+
+//Get information from JSON
+$.ajax({
+   url: "chart.json",
+   dataType: "text",
+   async: false,
+   success: function(data) {
+
+      var json = $.parseJSON(data);
+      for (var i = 0; i < json.numbers.length; i++) {
+         labelValue.push(json.numbers[i].month);
+         axisYValue.push(json.numbers[i].money);
+         axisXValue.push(i);
+      }
+	  //Temporarily store info so that the function only has to run once 
+      var tempDataPointInfo = jsonToDataPoints(axisXValue, axisYValue, labelValue);
+	  dataPointsCover = tempDataPointInfo[0];
+	  dataPointsValue = tempDataPointInfo[1];
+	  maxYAxis = tempDataPointInfo[2];
+   }
+});
+
+//START OF CANVASJS PLUGIN
+var chart = new CanvasJS.Chart("chartContainer", {
+   axisX: {
+      gridThickness: 0,
+      viewportMinimum: viewportMin,
+      viewportMaximum: viewportMax,
+      lineColor: "transparent",
+      tickColor: "transparent"
+   },
+   
+   axisY: {
+      gridThickness: 0,
+      lineColor: "transparent",
+      tickColor: "transparent",
+      labelFontColor: "transparent",
+      maximum: (maxYAxis - 100)
+   },
+
+   toolTip: {
+      enabled: true,
+      fontColor: "#fff",
+      fontStyle: "normal",
+      cornerRadius: 100,
+      backgroundColor: "#53BBB3",
+      borderColor: "transparent",
+      markerBorderThickness: 0,
+      fillOpacity: 0,
+
+      contentFormatter: function(e) {
+         var content = " ";
+         for (var i = 0; i < e.entries.length; i++) {
+            content += "</div><div class='toolTipContainer'><div class='speechBubble'></div><span class='toSerif circleInfo'>i</span>" + e.entries[i].dataPoint.label + ", $" + e.entries[i].dataPoint.y + "</div>";
+            content += "<br/>";
+         }
+         return content;
+      }
+   },
+
+   backgroundColor: "#F9F9F9",
+   
+   //Data from JSON file is sent through here and given these attributes
+   data: [{
+      //Top White Background
+      highlightEnabled: false,
+      type: "area",
+      color: "#ffffff",
+      fillOpacity: 1,
+      lineThickness: 0,
+      markerBorderColor: "#fff",
+      markerColor: "#e1e8ee",
+      markerSize: 0,
+      markerBorderThickness: 0,
+      tooltipContent: null,
+
+      dataPoints: dataPointsCover
+   }, {
+      //Main Data
+      type: "line",
+      color: "#E1E8EE",
+      fillOpacity: 0.1,
+      lineThickness: 2,
+      markerBorderColor: "#ffffff",
+      markerColor: "#e1e8ee",
+      markerSize: 10,
+      markerBorderThickness: 2,
+      mouseover: onMouseover,
+
+      dataPoints: dataPointsValue
+   }, {
+      //Lower Line Background
+      type: "area",
+      color: "#F9F9F9",
+      fillOpacity: 1,
+      lineThickness: 2,
+      tooltipContent: null,
+      highlightEnabled: false,
+      markerSize: 0,
+
+      dataPoints: dataPointsValue
+   }]
+});
+//END OF CANVASJS PLUGIN
+
+verticalLineChart(chart.options.data[1].dataPoints, chart);
+
+chart.render();
+
+var scrollPane = $(".scroll-pane");
+var position = 0;
+
+var scrollbar = $(".scroll-bar")
+   .slider({
+      range: 'min',
+      min: 0,
+      max: chart.options.data[0].dataPoints.length,
+      value: chart.options.data[0].dataPoints.length / 2,
+      step: 1,
+      slide: function(event, ui) {
+         currentPos = ui.value;
+         chart.options.axisX.viewportMaximum = currentPos;
+
+         //slide down
+         if (currentPos <= 5) {
+            console.log("REACHED END LOWER");
+            chart.options.axisX.viewportMaximum = 12;
+         } else if (chart.options.axisX.viewportMaximum >= (chart.options.data[0].dataPoints.length - 12)) {
+            console.log("REACHED END TOP");
+            chart.options.axisX.viewportMaximum = chart.options.data[0].dataPoints.length - 1;
+            chart.options.axisX.viewportMinimum = chart.options.data[0].dataPoints.length - 12;
+         } else {
+            console.log("Working inbetween");
+            chart.options.axisX.viewportMinimum = currentPos - 6;
+            chart.options.axisX.viewportMaximum = currentPos + 6;
+         }
+
+         chart.render();
+      }
+   });
+
+var handleHelper = scrollbar.find(".ui-slider-handle").append("<span class='ui-icon ui-icon-grip-dotted-vertical noUi-handle'><span class='verticalLines'>lorem</span></span>").wrap("<div class='ui-handle-helper-parent'></div>").parent();
+
+scrollPane.css("overflow", "hidden");
+
+   
    $("#slides").slidesjs({
       width: 940,
       height: 500,
@@ -244,241 +475,4 @@ $(window).resize(function() {
          marginTop: 20
       })
    }
-});
-   
-
-$(window).on("load", function() {
-   $(function() {
-      var axisYValue = [];
-      var axisXValue = [];
-      var labelValue = [];
-      var dataPointsCover = [];
-      var dataPointsValue = [];
-      var maxYAxis = 0;
-
-      //Get information from JSON
-      $.ajax({
-         url: "chart.json",
-         dataType: "text",
-         async: false,
-         success: function(data) {
-
-            var json = $.parseJSON(data);
-            for (var i = 0; i < json.numbers.length; i++) {
-               labelValue.push(json.numbers[i].month);
-               axisYValue.push(json.numbers[i].money);
-               axisXValue.push(i);
-            }
-            jsonToDataPoints(axisXValue, axisYValue, labelValue);
-         }
-      });
-
-      function jsonToDataPoints(axisX, axisY, labelFormat) {
-         maxYAxis = maxNumber(axisY) + 200;
-         for (var i = 0; i < axisX.length; i++) {
-            //DataPoints for color change of entire background
-            dataPointsCover.push({
-               y: maxYAxis,
-               x: axisX[i]
-            });
-
-            //DataPoints for main section
-            dataPointsValue.push({
-               y: axisY[i],
-               x: axisX[i],
-               label: labelFormat[i]
-            });
-         }
-      }
-
-      function maxNumber(arrayNumbers) {
-         var highest = -1;
-         for (var i = 0; i < arrayNumbers.length; i++) {
-            if (arrayNumbers[i] > highest) {
-               highest = arrayNumbers[i];
-            }
-         };
-         return highest;
-      }
-
-      var viewportMin = 0;
-      var viewportMax = 11;
-      var currentPos = 0;
-
-      var chart = new CanvasJS.Chart("chartContainer", {
-         axisX: {
-            gridThickness: 0,
-            viewportMinimum: viewportMin,
-            viewportMaximum: viewportMax,
-            lineColor: "transparent",
-            tickColor: "transparent"
-         },
-         axisY: {
-            gridThickness: 0,
-            lineColor: "transparent",
-            tickColor: "transparent",
-            labelFontColor: "transparent",
-            maximum: (maxYAxis - 100)
-         },
-
-         toolTip: {
-            enabled: true,
-            fontColor: "#fff",
-            fontStyle: "normal",
-            cornerRadius: 100,
-            //53BBB3
-            backgroundColor: "#53BBB3",
-            borderColor: "transparent",
-            markerBorderThickness: 0,
-            fillOpacity: 0,
-
-            contentFormatter: function(e) {
-               var content = " ";
-               for (var i = 0; i < e.entries.length; i++) {
-                  content += "</div><div class='toolTipContainer'><div class='speechBubble'></div><span class='toSerif circleInfo'>i</span>" + e.entries[i].dataPoint.label + ", $" + e.entries[i].dataPoint.y + "</div>";
-                  content += "<br/>";
-               }
-               return content;
-            }
-         },
-
-         backgroundColor: "#F9F9F9",
-         data: [{
-            //Top White Background
-            highlightEnabled: false,
-            type: "area",
-            color: "white",
-            fillOpacity: 1,
-            lineThickness: 0,
-            markerBorderColor: "#fff",
-            markerColor: "#e1e8ee",
-            markerSize: 0,
-            markerBorderThickness: 0,
-            tooltipContent: null,
-
-            dataPoints: dataPointsCover
-         }, {
-            //Main Data
-            type: "line",
-            color: "#E1E8EE",
-            fillOpacity: 0.1,
-            lineThickness: 2,
-            markerBorderColor: "#fff",
-            markerColor: "#e1e8ee",
-            markerSize: 10,
-            markerBorderThickness: 2,
-            mouseover: onMouseover,
-
-            dataPoints: dataPointsValue
-         }, {
-            //Lower Line Background
-            type: "area",
-            color: "#F9F9F9",
-            fillOpacity: 1,
-            lineThickness: 2,
-            tooltipContent: null,
-            highlightEnabled: false,
-            markerSize: 0,
-
-            dataPoints: dataPointsValue
-         }]
-      });
-
-      verticalLineChart(chart.options.data[1].dataPoints);
-      //Vertical Lines and fill
-      function onMouseover(e) {
-         e.dataPoint.markerColor = "#53BBB3";
-         e.dataPoint.markerBorderThickness = 0;
-      }
-
-      function verticalLineChart(arrayDataPoints) {
-         var axisY = [];
-         var axisX = [];
-         var dataInsert = [];
-
-         for (var i = 0; i < arrayDataPoints.length; i++) {
-            axisX[i] = arrayDataPoints[i].x;
-            dataInsert[i] = {
-               type: "line",
-               lineColor: "#e8e9e9",
-               lineThickness: 1,
-               markerBorderThickness: false,
-               markerSize: 0,
-               tooltipContent: null,
-
-               dataPoints: [
-
-                  {
-                     y: arrayDataPoints[i].y,
-                     x: arrayDataPoints[i].x,
-                     highlightEnabled: false,
-                     toolTipContent: null,
-                     lineDashType: "longDash"
-                  }, {
-                     y: 0,
-                     x: arrayDataPoints[i].x,
-                     highlightEnabled: false,
-                     toolTipContent: null
-                  },
-               ]
-            };
-            chart.options.data.push(dataInsert[i]);
-         }
-      };
-      //datapoint Hover
-
-      function test(e) {
-         var className = '.' + e.dataPoint.name;
-         var leftX = event.pageX - ($(className).outerWidth() / 2);
-         var topY = event.pageY - $(className).outerHeight() - 40;
-         $(className).css({
-            top: topY,
-            left: leftX
-         }).show('fast');
-      }
-
-      function removeHover(e) {
-         var className = '.' + e.dataPoint.name;
-         $(className).hide('fast');
-      }
-
-
-      chart.render();
-      var scrollPane = $(".scroll-pane");
-      var position = 0;
-
-      var scrollbar = $(".scroll-bar").slider({
-         range: 'min',
-         min: 0,
-         max: chart.options.data[0].dataPoints.length,
-         value: chart.options.data[0].dataPoints.length / 2,
-         step: 1,
-         slide: function(event, ui) {
-            currentPos = ui.value;
-            chart.options.axisX.viewportMaximum = currentPos;
-
-            //slide down
-            if (currentPos <= 5) {
-               console.log("REACHED END LOWER");
-               chart.options.axisX.viewportMaximum = 12;
-            } else if (chart.options.axisX.viewportMaximum >= (chart.options.data[0].dataPoints.length - 12)) {
-               console.log("REACHED END TOP");
-               chart.options.axisX.viewportMaximum = chart.options.data[0].dataPoints.length - 1;
-               chart.options.axisX.viewportMinimum = chart.options.data[0].dataPoints.length - 12;
-            } else {
-               console.log("Working inbetween");
-               chart.options.axisX.viewportMinimum = currentPos - 6;
-               chart.options.axisX.viewportMaximum = currentPos + 6;
-            }
-
-            chart.render();
-         }
-      });
-
-      var handleHelper = scrollbar.find(".ui-slider-handle")
-         .append("<span class='ui-icon ui-icon-grip-dotted-vertical noUi-handle'><span class='verticalLines'>lorem</span></span>")
-         .wrap("<div class='ui-handle-helper-parent'></div>").parent();
-
-      scrollPane.css("overflow", "hidden");
-   });
 });
